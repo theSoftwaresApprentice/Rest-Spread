a651353c695408458046df141e8b247acc69d483
theSoftwaresApprentice <mitchell.e.nelson@gmail.com>
2022-10-29 13:50:41 -0400

    exercise upload

---
+++ rest-spread-ex.js
@@ -0,0 +1,107 @@
+// Refactor
+// function filterOutOdds() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.filter(function(num) {
+//       return num % 2 === 0
+//     });
+//   }
+
+const filterOutOdds = (...nums) => 
+    nums.filter(num => num % 2 === 0); //added in extra parenz around final function
+
+
+
+// findMin
+// Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
+
+// Make sure to do this using the rest and spread operator.
+
+// findMin(1,4,12,-3) // -3
+// findMin(1,-1) // -1
+// findMin(3,1) // 1
+
+const findMin = (...nums) => 
+    nums.reduce((smallest, next) => 
+        {if (smallest < next){return smallest}
+        return next;})
+//Answer = const findMin = (...args) => Math.min(...args)
+
+
+// mergeObjects
+// Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+
+// mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+
+const mergeObjects = (obj1, obj2) => ({ ... obj1, ... obj2 });
+
+// doubleAndReturnArgs
+// Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+
+// doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
+// doubleAndReturnArgs([2],10,4) // [2, 20, 8]
+
+const doubleAndReturnArgs = (arr, ...string) => {
+    const stringMapped = string.map(num => num*2);
+    return [...arr, ...stringMapped];
+}
+//Answer = const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(v => v *2)]
+
+
+// Slice and Dice!
+// For this section, write the following functions using rest, spread and refactor these functions to be arrow functions!
+
+// Make sure that you are always returning a new array or object and not modifying the existing inputs.
+
+
+// function removeRandom(items) {
+// }
+const removeRandom = items => {
+    removalInd = Math.floor(Math.random()*items.length);
+    return [...items.slice(0, removalInd), ...items.slice(removalInd+1)]
+}
+
+/** Return a new array with every item in array1 and array2. */
+// function extend(array1, array2) {}
+const extend = (array1, array2) => {
+    return [... array1, ...array2];
+}
+
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+// function addKeyVal(obj, key, val) {}
+const addKeyVal = (obj, key, val) => {
+    newObj = { ...obj, key: val};
+    return newObj;
+}
+
+
+/** Return a new object with a key removed. */
+
+// function removeKey(obj, key) {}
+const removeKey = (obj, key) => {
+    newObj = {...obj};
+    delete newObj.key;
+    return newObj;
+
+}
+
+
+/** Combine two objects and return a new object. */
+
+// function combine(obj1, obj2) {}
+const combine = (obj1, obj2) => {
+    newObj = {...obj1, ...obj2};
+    return newObj;
+}
+
+
+/** Return a new object with a modified key and value. */
+
+// function update(obj, key, val) {}
+const update = (obj, key, val) => {
+    const newObj = {...obj};
+    newObj.key = val;
+    return newObj;
+}
